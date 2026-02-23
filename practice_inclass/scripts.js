6d509353843ed70e6793cb0c5923123a91078158
karimova-a <a_karimova@kbtu.kz>
2026-02-23 10:39:37 +0500

    Add Lab_4

---
+++ practice_inclass/scripts.js
@@ -0,0 +1,13 @@
+const colorbtn = document.querySelectorAll('color-btn');
+const constbtn = document.getElementById('const-btn');
+const resetbtn = document.getElementById('reset-btn');
+const colorcod = document.getElementById('color-code') ;
+const bodyel= document.body ;
+
+function changeBckcolor(color) {
+    onclick(colorbtn).document.body.style.backgroundColor = color;
+
+}
+
+
+
