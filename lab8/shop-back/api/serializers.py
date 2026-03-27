06293c8cbe8f9e49b75d20951b8a46404ec14b56
karimova-a <a_karimova@kbtu.kz>
2026-03-28 03:39:19 +0500

    lab8

---
+++ lab8/shop-back/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class ProductSerializer(serializers.ModelSerializer):
+    # Optional: include category name instead of just ID
+    category_name = serializers.ReadOnlyField(source='category.name')
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price', 'description', 'count', 'is_active', 'category', 'category_name']
